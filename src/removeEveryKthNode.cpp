28a10bd0a10453a0fdb9342b2db5765938886996
venkat <venkat>
2015-11-24 11:24:01 +0530

    Submit Commit

--- src/removeEveryKthNode.cpp
+++ src/removeEveryKthNode.cpp
@@ -12,12 +12,37 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * removeEveryKthNode(struct node *head, int K) {
-	return NULL;
+	if ((head == NULL) || (K == 1)){
+		return NULL;
+	}
+	struct node *cur;
+	int num = K;
+	cur = (struct node*)malloc(sizeof(struct node));
+	cur = head;
+	while ((cur != NULL)){
+		if ((num == 2) && (cur->next != NULL)){
+			if ((cur->next)->next == NULL){
+				cur->next = NULL;
+			}
+			else{
+				cur->next = (cur->next)->next;
+			}
+			num = K + 1;
+		}
+		cur = cur->next;
+		num--;
+	}
+	if ((K > 0) && (cur == NULL)){
+		return head;
+	}
+	else{
+		return NULL;
+	}
 }
