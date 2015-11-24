a8e58e4bc11a57440a7feacf67f36af20ac9497a
venkat <venkat>
2015-11-24 12:07:16 +0530

    Submit Commit

--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -12,12 +12,34 @@ NOTES: Without using extra array or linked list.
 */
 
 #include <stdio.h>
-
+void swap(struct node*, struct node*);
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * sortLinkedList(struct node *head) {
-	return NULL;
+	struct node *ptr1;
+	struct node *ptr2 ;
+	if (head == NULL){
+		return NULL;
+	}
+	ptr1 = head;
+	while (ptr1->next != NULL){
+		ptr2 = head;
+		while (ptr2->next != NULL){
+			if (ptr2->num > (ptr2->next)->num){
+				swap(ptr2, ptr2->next);
+			}
+			ptr2 = ptr2->next;
+		}
+		ptr1 = ptr1->next;
+	}
+	return head;
+}
+void swap(struct node *a, struct node *b)
+{
+	int temp = a->num;
+	a->num = b->num;
+	b->num = temp;
 }
